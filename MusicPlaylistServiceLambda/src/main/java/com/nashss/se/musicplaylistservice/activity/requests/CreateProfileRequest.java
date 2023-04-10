6cdbb63efdc08bb59cf27a0a8b05854a95af34f9
Noah Cain <nzcain30@gmail.com>
2023-04-10 15:00:33 -0500

    Further progress on CPR

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/CreateProfileRequest.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/CreateProfileRequest.java
@@ -1,6 +1,7 @@
 package com.nashss.se.musicplaylistservice.activity.requests;
 
 import java.time.ZonedDateTime;
+import java.util.Set;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
@@ -15,10 +16,62 @@ public class CreateProfileRequest {
     private final String gender;
     private final ZonedDateTime dateOfBirth;
     private final Set<Event> eventList;
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public ZonedDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Set<Event> getEventList() {
+        return eventList;
+    }
+
+    public Set<String> getFollowerList() {
+        return followerList;
+    }
+
     private final Set<String> followerList;
 
-    private CreateProfileRequest(boolean isNew, String firstName, String lastName, String emailAddress,String location
-    String gender, ZonedDateTime dateOfBirth,Set<Event> eventList,Set<String> followerList){
+    @Override
+    public String toString() {
+        return "CreateProfileRequest{" +
+                "isNew=" + isNew +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", location='" + location + '\'' +
+                ", gender='" + gender + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", eventList=" + eventList +
+                ", followerList=" + followerList +
+                '}';
+    }
+
+    private CreateProfileRequest(boolean isNew, String firstName, String lastName, String emailAddress, String location,
+                                 String gender, ZonedDateTime dateOfBirth, Set<Event> eventList, Set<String> followerList){
         this.isNew = isNew;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -29,12 +82,48 @@ public class CreateProfileRequest {
         this.eventList = eventList;
         this.followerList = followerList;
     }
-    public String getFirstName(){
-        return firstName;
+@JsonPOJOBuilder
+    public static class Builder{
+    private  boolean isNew;
+    private  String firstName;
+    private  String lastName;
+    private  String emailAddress;
+    private  String location;
+    private  String gender;
+    private  ZonedDateTime dateOfBirth;
+    private  Set<Event> eventList;
+
+    public Builder withFirstName(String firstName){
+        this.firstName = firstName;
+        return this;
     }
-    public String getLastName(){
-        return lastName;
+    public Builder withLastName(String lastName){
+        this.lastName = lastName;
+        return this;
     }
+    public Builder withEmailAddress(String emailAddress){
+        this.emailAddress = emailAddress;
+        return this;
+    }
+    public Builder withLocation(String location){
+        this.location = location;
+        return this;
+    }
+    public Builder withGender(String gender){
+        this.gender = gender;
+        return this;
+    }
+    public Builder withDateOfBirth(ZonedDateTime dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+        return this;
+    }
+    public Builder withEvents(Set<Event> eventList){
+        this.eventList = eventList;
+        return this;
+    }
+
+
+}
 
     
 }
