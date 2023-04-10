88a14457a5844627d65aed365b4202c2aa43b592
Noah Cain <nzcain30@gmail.com>
2023-04-10 13:23:13 -0500

    Further additions to CreateProfileRequest

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/CreateProfileRequest.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/CreateProfileRequest.java
@@ -1,12 +1,40 @@
 package com.nashss.se.musicplaylistservice.activity.requests;
-i
-mport com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.time.ZonedDateTime;
+
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 @JsonDeserialize(builder = CreateProfileRequest.Builder.class)
 public class CreateProfileRequest {
     private final boolean isNew;
-    private final String 
+    private final String firstName;
+    private final String lastName;
+    private final String emailAddress;
+    private final String location;
+    private final String gender;
+    private final ZonedDateTime dateOfBirth;
+    private final Set<Event> eventList;
+    private final Set<String> followerList;
+
+    private CreateProfileRequest(boolean isNew, String firstName, String lastName, String emailAddress,String location
+    String gender, ZonedDateTime dateOfBirth,Set<Event> eventList,Set<String> followerList){
+        this.isNew = isNew;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.location = location;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.eventList = eventList;
+        this.followerList = followerList;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+
     
 }
