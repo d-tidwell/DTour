29be3d3522d9821002f7dc5dc2236b75e7bfb0f7
Andres A <anfearpri@gmail.com>
2023-04-10 11:58:08 -0500

    created profile dao and modified metrics constants

---
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/dynamodb/ProfileDao.java
@@ -0,0 +1,35 @@
+package com.nashss.se.musicplaylistservice.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.nashss.se.musicplaylistservice.dynamodb.models.Profile;
+import com.nashss.se.musicplaylistservice.metrics.MetricsConstants;
+import com.nashss.se.musicplaylistservice.metrics.MetricsPublisher;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class ProfileDao {
+    private final DynamoDBMapper dynamoDbMapper;
+    private final MetricsPublisher metricsPublisher;
+
+    @Inject
+    public ProfileDao(DynamoDBMapper dynamoDbMapper, MetricsPublisher metricsPublisher) {
+        this.dynamoDbMapper = dynamoDbMapper;
+        this.metricsPublisher = metricsPublisher;
+    }
+
+    public Profile getProfile(String id){
+
+        Profile profile = this.dynamoDbMapper.load(Profile.class, id);
+
+        if(profile == null){
+            metricsPublisher.addCount(MetricsConstants.GETPROFILE_PROFILENOTFOUND_COUNT, 1);
+//            throw new
+        }
+
+        return null;
+    }
+
+
+}
