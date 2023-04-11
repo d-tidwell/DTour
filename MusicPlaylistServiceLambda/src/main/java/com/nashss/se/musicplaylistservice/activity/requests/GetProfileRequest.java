fec4ff409c26d1029078794d842dc90b9dd14ab1
Andres A <anfearpri@gmail.com>
2023-04-11 15:54:07 -0500

    created get profile request

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/GetProfileRequest.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/GetProfileRequest.java
@@ -1,4 +1,45 @@
 package com.nashss.se.musicplaylistservice.activity.requests;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.nashss.se.musicplaylistservice.activity.results.GetProfileResult;
+
+@JsonDeserialize(builder = GetProfileResult.Builder.class)
 public class GetProfileRequest {
+
+    private final String profileId;
+
+    public GetProfileRequest(String profileId) {
+        this.profileId = profileId;
+    }
+
+    public String getProfileId() {
+        return profileId;
+    }
+
+    @Override
+    public String toString() {
+        return "GetProfileRequest{" +
+                "profileId='" + profileId + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+
+        private String profileId;
+
+        public GetProfileRequest.Builder withId(String profileId) {
+            this.profileId = profileId;
+            return this;
+        }
+
+        public GetProfileRequest build(){
+            return new GetProfileRequest(profileId);
+        }
+
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
 }
--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/results/GetProfileResult.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/results/GetProfileResult.java
@@ -41,4 +41,8 @@ public class GetProfileResult {
 
     }
 
+    public static Builder builder(){
+        return new Builder();
+    }
+
 }
