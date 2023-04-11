9f0c571136e49c89c2dc81efd797b8c241dce459
Ashley Gilday <ashleybrookrobinson@gmail.com>
2023-04-11 15:41:14 -0500

    created EventNotFoundException class

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/metrics/MetricsConstants.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/metrics/MetricsConstants.java
@@ -4,7 +4,7 @@ package com.nashss.se.musicplaylistservice.metrics;
  * Constant values for use with metrics.
  */
 public class MetricsConstants {
-    public static final String GETEVENT_PLAYLISTNOTFOUND_COUNT = "GetEvent.EventNotFoundException.Count";
+    public static final String GETEVENT_EVENTNOTFOUND_COUNT = "GetEvent.EventNotFoundException.Count";
     public static final String UPDATEEVENT_INVALIDATTRIBUTEVALUE_COUNT =
         "UpdateEvent.InvalidAttributeValueException.Count";
     public static final String UPDATEEVENT_INVALIDATTRIBUTECHANGE_COUNT =
