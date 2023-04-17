b3ab4c7ca6d06e12709e8d6112c7d9922dc2ef3d
dtidwell <conversations.and.L@gmail.com>
2023-04-16 19:16:36 -0500

    logout id for profile

---
+++ web/src/pages/landingPage.js
@@ -0,0 +1,5 @@
+import dannaClient from '../api/dannaClient';
+import BindingClass from "../util/bindingClass";
+import Header from '../components/dannaHeader';
+import DataStore from "../util/DataStore";
+
