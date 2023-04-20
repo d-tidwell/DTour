b43b04a9cfa06f6fc821c19434a360c4ec34415e
dtidwell <conversations.and.L@gmail.com>
2023-04-20 13:36:12 -0500

    qoute

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/RemoveEventFromProfileRequest.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/RemoveEventFromProfileRequest.java
@@ -1,25 +1,25 @@
 package com.nashss.se.musicplaylistservice.activity.requests;
-
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
-@JsonDeserialize(builder= RemoveEventFromProfileRequest.class)
+
+@JsonDeserialize(builder = RemoveEventFromProfileRequest.Builder.class)
 public class RemoveEventFromProfileRequest {
+
     private final String profileId;
     private final String eventId;
 
-    private RemoveEventFromProfileRequest(String profileId, String eventId){
+
+    public RemoveEventFromProfileRequest(String profileId, String eventId) {
         this.profileId = profileId;
         this.eventId = eventId;
-    }
 
-    public String getProfileId() {
-        return profileId;
     }
 
     public String getEventId() {
         return eventId;
     }
+    public String getProfileId() {return profileId;}
 
     @Override
     public String toString() {
@@ -29,27 +29,30 @@ public class RemoveEventFromProfileRequest {
                 '}';
     }
 
-    //CHECKSTYLE:OFF:Builder
-
-    public static Builder builder(){
-        return new Builder();
-    }
     @JsonPOJOBuilder
-    public static class Builder{
+    public static class Builder {
 
         private String profileId;
         private String eventId;
 
-        public Builder withProfileId(String profileId){
+        public Builder withProfileId(String profileId) {
             this.profileId = profileId;
             return this;
         }
-        public Builder withEventId(String eventId){
+
+        public Builder withEventId(String eventId) {
             this.eventId = eventId;
             return this;
         }
-        public RemoveEventFromProfileRequest build(){
-            return new RemoveEventFromProfileRequest(profileId,eventId);
+
+        public RemoveEventFromProfileRequest build() {
+            return new RemoveEventFromProfileRequest(profileId, eventId);
         }
+
     }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
 }
